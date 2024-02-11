91884f77f588870d0089a66ff2277d38b562d827
Liam Le <154930237+liamlecs@users.noreply.github.com>
2024-02-11 09:55:12 -0600

    Update codered.js

--- codered.js
+++ codered.js
@@ -15,3 +15,23 @@ function turnOffPreviousButton() {
     previousButton.classList.remove('is-toggled');
   }
 }
+
+document.getElementById('contactForm').addEventListener('submit', function(event) {
+  var emailInput = document.getElementById('email');
+  var email = emailInput.value.trim();
+
+  if (!isValidEmail(email)) {
+      event.preventDefault(); // Prevent form submission
+      alert('Invalid email address!');
+      emailInput.focus(); // Set focus back to the email input field
+  }
+  else if(isValidEmail(email)) {
+    alert('Email sent successfully!')
+  }
+});
+
+function isValidEmail(email) {
+  // Basic email validation using a regular expression
+  var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return emailRegex.test(email);
+}
